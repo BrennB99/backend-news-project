035041dcb090a1d16d15787afe64f59f94eb12a9
BrennB99 <ben.brennan249@gmail.com>
2022-08-01 16:17:11 +0100

    Added PATCH article_id with errors handled

--- app.js
+++ app.js
@@ -4,19 +4,24 @@ const {
   getTopics,
   invalidEndpoint,
   getArticle,
+  updateArticle,
 } = require("./controllers/app.controllers.js");
+
+app.use(express.json());
 
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticle);
+app.patch("/api/articles/:article_id", updateArticle);
 
 app.get("*", invalidEndpoint);
 
 app.use((err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Invalid ID!" });
+  } else {
+    res.status(err.status).send({ msg: err.msg });
   }
-  res.status(err.status).send({ msg: err.msg });
 });
 
 module.exports = app;
--- models/app.models.js
+++ models/app.models.js
@@ -16,3 +16,21 @@ exports.retrieveArticle = (article_id) => {
       return rows[0];
     });
 };
+
+exports.changeArticle = (articleInfo) => {
+  const { article_id, inc_votes } = articleInfo;
+  const infoArray = [article_id, inc_votes];
+
+  if (inc_votes === undefined || isNaN(inc_votes)) {
+    return Promise.reject({ status: 400, msg: "Invalid Input!" });
+  }
+
+  return db
+    .query(
+      `UPDATE articles SET votes = votes + $2 WHERE article_id = $1 RETURNING *`,
+      infoArray
+    )
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};

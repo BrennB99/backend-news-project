b367e50914f8cc9ecdd9baedd090f23b9c6dbc6c
BrennB99 <ben.brennan249@gmail.com>
2022-08-01 14:53:23 +0100

    change 'expect article_id = any(Number)' to 'expect article_id = 1'

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -59,7 +59,7 @@ describe("GET /api/articles/:article_id", () => {
           expect.objectContaining({
             author: expect.any(String),
             title: expect.any(String),
-            article_id: expect.any(Number),
+            article_id: 1,
             body: expect.any(String),
             topic: expect.any(String),
             created_at: expect.any(String),
